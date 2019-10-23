7a3684e24e48bac4d74c1a617a21f9acba1f43d8
Tytus <tytusnawara@gmail.com>
2019-10-23 14:17:25 +0200

    movement, camera follows player

--- Assets/Scripts/Fleet.cs
+++ Assets/Scripts/Fleet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Fleet : MonoBehaviour
 {
     public GameObject shipPrefab;
+    public float speed = 0.1f;
     List<GameObject> ships = new List<GameObject>();
     Vector2 range = new Vector2(5, 5);
     int shipsAtTheStart = 20;
@@ -28,6 +29,9 @@ public class Fleet : MonoBehaviour
             ships.Add(Instantiate(shipPrefab, 
                 new Vector2(Random.RandomRange(-range.x, range.x), Random.RandomRange(-range.y, range.y)),
                 Quaternion.identity));
+        foreach (var ship in ships) {
+            ship.transform.parent = transform;
+        }
     }
 
     // Update is called once per frame
@@ -42,7 +46,10 @@ public class Fleet : MonoBehaviour
         {
             Vector2 normalizeDirectionToCenter = (transform.position - ship.transform.position).normalized;
             Debug.DrawRay(ship.transform.position, normalizeDirectionToCenter, Color.red);
-            ship.GetComponent<Rigidbody2D>().velocity = normalizeDirectionToCenter* 1;
+            
+            Rigidbody2D rb = ship.GetComponent<Rigidbody2D>();
+            rb.velocity = normalizeDirectionToCenter * 1;
+            transform.position = (Vector2)transform.position + fleetFacingDirection*speed;
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour
     {
         playersFleet = Instantiate(fleetPrefab, transform.position, Quaternion.identity);
         joystickDirectionNormalized = new Vector2(1, 0);
+
     }
 
     // Update is called once per frame
@@ -20,19 +21,15 @@ public class Player : MonoBehaviour
     {
         if(joystick.Direction.magnitude > 0)
             joystickDirectionNormalized = joystick.Direction.normalized;
-        float angle = Vector3.Angle(new Vector3(0.0f, 1.0f, 0.0f), joystickDirectionNormalized);
-        if (joystickDirectionNormalized.x < 0.0f)
-        {
-            angle = -angle;
-            angle = angle + 360;
-        }
+     
         //Vector2 lookDirection = (Vector2)transform.position + joystick.Direction.normalized;
         //transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.forward);
         //Vector3 point = new Vector3(5, 0, 0);
         //Vector3 axis = new Vector3(0, 0, 1);
         //transform.RotateAround(transform.position, axis, angle);
-        transform.rotation = Quaternion.Euler(Vector3.forward * (-angle));
+        
         playersFleet.GetComponent<Fleet>().faceFleetInDirection(joystickDirectionNormalized);
-
+        transform.position = playersFleet.transform.position;
     }
+   
 }
